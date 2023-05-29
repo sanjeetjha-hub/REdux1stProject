492507f67c85c5b777eb008be499c9b128b2c861
Sanjeet Uday Jha <sanjeetj@cybage.com>
2023-05-29 18:22:53 +0530

    working on multiple reducer

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from 'redux-logger';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import logger from "redux-logger";
 import thunk from "redux-thunk";
-import axios from 'axios';
+import axios from "axios";
 
+//action name const
+const ini = "init";
+const inc = "increment";
+const dec = "decrement";
+const incByAmount = "incrementByAmount";
 
-
-
-//action name const 
-const ini = 'init'
-const inc = 'increment';
-const dec = 'decrement';
-const incByAmount = 'incrementByAmount'
-
-const store = createStore(reducer, applyMiddleware(logger.default, thunk.default));
+const store = createStore(
+  combineReducers({ account: accountReducer, bonus: bonusReducer }),
+  applyMiddleware(logger.default, thunk.default)
+);
 
 const history = [];
 
+function accountReducer(state = { points: 1 }, action) {
+  switch (action.type) {
+    case ini:
+      return { amount: action.payload };
+    case inc:
+      return { amount: state.amount + 1 };
+    case dec:
+      return { amount: state.amount - 1 };
+    case incByAmount:
+      return { amount: state.amount + action.payload };
+    default:
+      return state;
+  }
+}
 
-
-function reducer(state = { amount: 1 }, action) {
-    switch (action.type) {
-        case ini:
-            return { amount: action.payload }
-        case inc:
-            return { amount: state.amount + 1 }
-        case dec:
-            return { amount: state.amount - 1 }
-        case incByAmount:
-            return { amount: state.amount + action.payload }
-        default:
-            return state;
-    }
-
+function bonusReducer(state = { amount: 1 }, action) {
+  switch (action.type) {
+    case inc:
+      return { points: state.points + 1 };
+    default:
+      return state;
+  }
 }
 
 // global state
@@ -41,28 +47,29 @@ function reducer(state = { amount: 1 }, action) {
 //     console.log(history)
 // })
 
-
 //Action creator
-async function getUser(dispatch, getState) {
-    const { data } = await axios.get('');
+function getUser(id) {
+  return async (dispatch, getState) => {
+    const { data } = await axios.get("");
     dispatch(initUser(data.amount));
+  };
 }
 
 function initUser(value) {
-    return { type: ini, payload: value }
+  return { type: ini, payload: value };
 }
 function increment() {
-    return { type: inc }
+  return { type: inc };
 }
 function decrement() {
-    return { type: dec }
-} function incrementByAmount(value) {
-    return { type: incByAmount, payload: value }
+  return { type: dec };
+}
+function incrementByAmount(value) {
+  return { type: incByAmount, payload: value };
 }
 
 setInterval(() => {
-    store.dispatch(initUser(500))
-}, 2000)
-
+  store.dispatch(getUser(1));
+}, 2000);
 
 //Async Api call
